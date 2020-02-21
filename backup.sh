1c4060c9989e2f5a181a3d360c02c1ccfd1e7459
ferdinandpolpol <ferdinand.polpol@gmail.com>
2020-02-21 11:43:44 +0800

    add oscar backups

--- backup.sh
+++ backup.sh
@@ -25,18 +25,26 @@ fi
 
 site=$(docker ps --format '{{.Names}}' | grep _backup_ | cut -d'_' -f1) 
 filename=$site.$(date +%Y%m%d-%H%M%S).sql
+documentName=$site.$(date +%Y%m%d-%H%M%S)-OscarDocument
 folder=$(date +%Y%m)
 
 rm -rf /dump
 rm -f /dump.lrz
+rm -rf /documentsBackup
+rm -f documents.tar
 
 docker exec -t ${site}_db_1 rm -fr /dump
 docker exec -t ${site}_db_1 $BACKUP_CMD
 docker cp ${site}_db_1:/dump /dump
+docker cp ${site}_tomcat_oscar_1:/var/lib/OscarDocument /documentsBackup/OscarDocument
 
 tar cvf /dump.tar /dump
 lrzip /dump.tar
 rm -r /dump
+tar cvf /documents.tar /documentsBackup 
+lrzip /documents.tar
+rm -rf /documentsBackup
 
 aws s3 mv /dump.tar.lrz s3://$BACKUP_BUCKET/$site/$folder/$filename.tar.lrz
+aws s3 mv /documents.tar s3://$BACKUP_BUCKET/$site/$folder/$documentName.tar.lrz
 
